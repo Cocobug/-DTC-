1d450df329a577b9ae728c1ce7326214e7b6c473
Malphaet <max.rigaut@gmail.com>
2012-01-29 06:23:39 +0100

    Working stuff out

--- engine.py
+++ engine.py
@@ -19,31 +19,66 @@
 # along with dtc-engine. If not, see <http://www.gnu.org/licenses/>.
 
 #----------------------+
-# Initialisation       |
+# Importation          |
 #- - - - - - - - - - - +
+
+import os,sys
 from bottle import Bottle, run, static_file, route, abort, redirect
 from bottle import jinja2_view as view, jinja2_template as template
+
+#----------------------+
+# Initialisation       |
+#- - - - - - - - - - - +
+
+templates={'global':'global/global.liquid','news':'news/news.liquid'}
+css={'global':'global/global.css','news':'global/news.css'}
+
 #======================+
 # Fonctions internes   |
 #----------------------+
+
+def model(category):
+	if category in templates and os.exists(templates[category]):
+		templates[category]
+	return templates['global']
+
+def article(filepath):
+	temp='pages/'+filepath+'.md'
+	if os.exists(temp): return temp
+	abort(404, 'Thou are weak')
+
+def css(category):
+	if category in css and os.exists(css[category]):
+		return css[category]
+	return css['global']
+
+#======================+
+# Programme principal  |
+#----------------------+
+
 app = Bottle()
 
 @app.route('/')
 @app.route('/hello')
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 @app.route('/images/<filename:re:.*\.png>#')
 def send_image(filename):
-    return static_file(filename, root='/path/to/image/files', mimetype='image/png')
+	return static_file(filename, root='/path/to/image/files', mimetype='image/png')
 
 @app.route('/static/<filename:path>')
 def send_static(filename):
-    return static_file(filename, root='/path/to/static/files')
+	return static_file(filename, root='/path/to/static/files')
 
-@app.error(404)
-def error404(error):
-    return 'Nothing here, sorry'
+@app.route('/news/<category>/<int:date>/name')
+def news(date,name):
+	pagename=name
+	header=''
+	footer=''
+	content='/news/'+category+'/'+date+'-'+name'
+	with open ar('news') as article: text=article.read()
+	template(model('news'),header=deader,footer=footer,content=text)
 
 app.debug=True
 run(app, reloader=True, host='localhost', port=8080)
